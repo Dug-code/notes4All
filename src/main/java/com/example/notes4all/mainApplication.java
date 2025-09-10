8aeb7abf3ea7c72051a8238c8b70e64a5f28b2f3
Doug Bluemel <douglas.bluemel@gmail.com>
2025-09-10 08:54:24 -0400

    simple name changes

--- src/main/java/com/example/notes4all/mainApplication.java
+++ src/main/java/com/example/notes4all/mainApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class mainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(mainApplication.class.getResource("welcome-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Welcome!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/notes4all/welcomeController.java
+++ src/main/java/com/example/notes4all/welcomeController.java
@@ -3,12 +3,12 @@ package com.example.notes4all;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class welcomeController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to Notes4All!");
     }
 }
